5b2b44ab4986624619c3780a7128ae2e94979fe1
Jake Smith <jake.smith@lexisnexis.com>
2013-05-22 13:01:10 +0100

    stack trace 'Memory limit exceeded'

--- roxie/roxiemem/roxiemem.cpp
+++ roxie/roxiemem/roxiemem.cpp
@@ -2461,6 +2461,7 @@ public:
                     if (numHeapPages == atomic_read(&totalHeapPages))
                     {
                         logctx.CTXLOG("RoxieMemMgr: Memory limit exceeded - current %u, requested %u, limit %u", pageCount, numRequested, pageLimit);
+                        PrintStackReport();
                         throw MakeStringException(ROXIEMM_MEMORY_LIMIT_EXCEEDED, "memory limit exceeded");
                     }
                 }
