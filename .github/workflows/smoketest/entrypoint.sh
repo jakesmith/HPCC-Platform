796022f053305d57c54ee930e023bf55b438b10d
Jake Smith <jakesmith@users.noreply.github.com>
2020-10-30 18:26:52 +0000

    multi-stage, cli

---
+++ .github/workflows/smoketest/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+runtype=$1
+source /hpcc-dev/hpccinstall/opt/HPCCSystems/sbin/hpcc_setenv
+/hpcc-dev/hpccinstall/opt/HPCCSystems/etc/init.d/hpcc-init start
+
+case ${runtype} in
+
+  setup)
+    ./ecl-test setup
+    ;;
+
+  *)
+    ./ecl-test run --pq 2 --target ${runtype} --excludeclass embedded,3rdparty,spray
+    ;;
+esac
+    
+
