68cb6ad5e0f717d7b9ee7d919efacecded24396f
Jake Smith <jakesmith@users.noreply.github.com>
2020-10-30 18:26:52 +0000

    multi-stage, cli

--- dali/datest/datest.cpp
+++ dali/datest/datest.cpp
@@ -3357,6 +3357,14 @@ int main(int argc, char* argv[])
 
     EnableSEHtoExceptionMapping();
 
+    printf("BUILD_VERSION_MAJOR=%u\n", BUILD_VERSION_MAJOR);
+    printf("BUILD_VERSION_MINOR=%u\n", BUILD_VERSION_MINOR);
+    printf("BUILD_VERSION_POINT=%u\n", BUILD_VERSION_POINT);
+    printf("BUILD_TAG=%s\n", BUILD_TAG);
+    printf("BUILD_TAG repeated4 =%s\n", BUILD_TAG);
+
+    return 0;
+
     try {
         StringBuffer cmd;
         splitFilename(argv[0], NULL, NULL, &cmd, NULL);
