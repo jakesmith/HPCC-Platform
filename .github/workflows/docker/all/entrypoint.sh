df851bed21712f7cd6f9101d15c005e1331e6fe3
Jake Smith <jake.smith@lexisnexisrisk.com>
2020-11-06 14:35:08 +0000

    wip

--- .github/workflows/docker/all/entrypoint.sh
+++ .github/workflows/docker/all/entrypoint.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-/opt/HPCCSystems/etc/init.d/hpcc-init start
-
-source /opt/HPCCSystems/sbin/hpcc_setenv
-cd /hpcc-dev/HPCC-Platform/testing/regress
+source /hpcc-dev/hpccinstall/opt/HPCCSystems/sbin/hpcc_setenv
+/hpcc-dev/hpccinstall/opt/HPCCSystems/etc/init.d/hpcc-init start
 
 ./ecl-test setup
-./ecl-test query --target thor --pq 2 --excludeclass embedded,3rdparty,spray sqagg*.ecl
+./ecl-test run --pq 2 --excludeclass embedded,3rdparty,spray
