99c63e5e248a9cc10fafe8470476e0f3d13be444
Jake Smith <jake.smith@lexisnexisrisk.com>
2020-11-09 11:58:35 +0000

    wip

---
+++ .github/workflows/smoketest/runregress.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+runtype=$1
+shift
+source /hpcc-dev/hpccinstall/opt/HPCCSystems/sbin/hpcc_setenv
+/hpcc-dev/hpccinstall/opt/HPCCSystems/etc/init.d/hpcc-init start
+
+./ecl-test query --pq 2 --target ${runtype} --excludeclass embedded,3rdparty,spray $@
+
