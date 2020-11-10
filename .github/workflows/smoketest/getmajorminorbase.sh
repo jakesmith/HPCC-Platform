e187e6abaf80a22ff8961a413d18665b82eb1727
Jake Smith <jake.smith@lexisnexisrisk.com>
2020-11-10 10:40:39 +0000

    wip

--- .github/workflows/smoketest/getmajorminorbase.sh
+++ .github/workflows/smoketest/getmajorminorbase.sh
@@ -7,7 +7,6 @@ starttag=$1 # e.g. master or candidate-7.12.x
 
 if [ "$starttag" = "master" ]; then
   result=$(git -c 'versionsort.suffix=-' ls-remote --exit-code --refs --sort='version:refname' --tags https://github.com/hpcc-systems/HPCC-Platform 'community_*.*.*' | egrep -e '\.0-1$' | tail -n 1 | sed -e 's/^.*\(community_.*\)$/\1/')
-  echo $?
 else
   regex="candidate-[0-9]+\.[0-9]+\..*"
   if [[ $starttag =~ $regex ]]; then
