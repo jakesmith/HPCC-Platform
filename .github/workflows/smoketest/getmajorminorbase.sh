2bd55c96c5b917f1e5658eaabaf29977adc71f6c
Jake Smith <jake.smith@lexisnexisrisk.com>
2020-11-10 12:54:15 +0000

    wip

--- .github/workflows/smoketest/getmajorminorbase.sh
+++ .github/workflows/smoketest/getmajorminorbase.sh
@@ -6,11 +6,15 @@ starttag=$1 # e.g. master or candidate-7.12.x
 # If $starttag is master, will chose most recent Major.Minor
 
 if [ "$starttag" = "master" ]; then
-  result=$(git -c 'versionsort.suffix=-' ls-remote --exit-code --refs --sort='version:refname' --tags https://github.com/hpcc-systems/HPCC-Platform 'community_*.*.*' | egrep -e '\.0-1$' | tail -n 1 | sed -e 's/^.*\(community_.*\)$/\1/')
+  result=$(git -c 'versionsort.suffix=-' ls-remote --exit-code --refs --sort='version:refname' --tags https://github.com/hpcc-systems/HPCC-Platform 'community_*.*.*' | tail -n 1 | sed -e 's/^.*\(community_.*\)$/\1/')
 else
+  # We don't have the branch yet, only the base_ref (e.g. candidate-7.12.x)
+  # If we had the head repo, we could do: git describe --abbrev=0 --tags
+
   regex="candidate-[0-9]+\.[0-9]+\..*"
   if [[ $starttag =~ $regex ]]; then
-    result=$(echo $starttag | sed -e 's/candidate-\([0-9]\+\.[0-9]\+\)\..*$/community_\1.0-1/')
+    findstart=$(echo $starttag | sed -e 's/candidate-\([0-9]\+\)\.\([0-9]\+\)\..*$/\1 \2/' | awk '{ print $1*100 + $2 + 0.999 }')
+    result=$(git -c 'versionsort.suffix=-' ls-remote --exit-code --refs --sort='version:refname' --tags https://github.com/hpcc-systems/HPCC-Platform 'community_*.*.*' | sed -e 's/^.*\(community_\)\([0-9]\+\)\.\([0-9]\+\)\.\([0-9]\+\)-\(.*\)$/\1 \2 \3 \4 \5/' | awk '{ if ($2*100+$3+$4/100 <= FINDSTART) { print "community_" $2 "." $3 "." $4 "-" $5 } }' FINDSTART=$findstart | tail -n 1)
   fi
 fi
 
