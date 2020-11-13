8a7a7b1d4a9dc8ba8a5a6164c993bda2a2f765ef
Jake Smith <jake.smith@lexisnexisrisk.com>
2020-11-13 14:23:30 +0000

    wip

--- .github/workflows/smoketest/entrypoint.sh
+++ .github/workflows/smoketest/entrypoint.sh
@@ -5,15 +5,5 @@ shift
 source /hpcc-dev/hpccinstall/opt/HPCCSystems/sbin/hpcc_setenv
 /hpcc-dev/hpccinstall/opt/HPCCSystems/etc/init.d/hpcc-init start
 
-case ${runtype} in
-
-  setup)
-    ./ecl-test setup $*
-    ;;
-
-  *)
-    ./ecl-test run --pq 2 --target ${runtype} --excludeclass embedded,3rdparty,spray $*
-    ;;
-esac
-    
+./ecl-test run --pq 2 --target ${runtype} --excludeclass embedded,3rdparty,spray $*
 
