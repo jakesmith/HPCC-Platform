b8093f0f2259078f14a91c979e2cd9160a954791
Jake Smith <jake.smith@lexisnexisrisk.com>
2021-01-22 15:39:32 +0000

    wip

--- dali/datest/datest.cpp
+++ dali/datest/datest.cpp
@@ -3358,6 +3358,8 @@ int main(int argc, char* argv[])
     EnableSEHtoExceptionMapping();
 
     try {
+ PROGLOG("here1");
+
         StringBuffer cmd;
         splitFilename(argv[0], NULL, NULL, &cmd, NULL);
         StringBuffer lf;
