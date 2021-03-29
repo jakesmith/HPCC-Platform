424f5a28fa3da169ba610c079197087590cf0f37
Jake Smith <jake.smith@lexisnexisrisk.com>
2021-03-29 11:45:59 +0100

    if then newline

--- dali/datest/datest.cpp
+++ dali/datest/datest.cpp
@@ -3334,6 +3334,14 @@ int main(int argc, char* argv[])
     ReleaseAtomBlock rABlock;
     InitModuleObjects();
 
+    /* test1 */
+    /* test2 */
+    /* test5 */
+    /* test6 */
+    /* v6 */
+    /* v7 */
+    /* v8 */
+
     EnableSEHtoExceptionMapping();
 
     try {
