491076d850438dbe3b5eb56acd482c40664249f4
Jake Smith <jake.smith@lexisnexisrisk.com>
2025-05-21 12:20:32 +0100

    extend iterateFilteredFiles to cope with IPT request for extensibility
    Signed-off-by: Jake Smith <jake.smith@lexisnexisrisk.com>

--- dali/sasha/saxref.cpp
+++ dali/sasha/saxref.cpp
@@ -2536,6 +2536,14 @@ public:
         unsigned defaultPersistExpireDays = props->getPropInt("@persistExpiryDefault", DEFAULT_PERSISTEXPIRYDAYS);
         StringArray expirylist;
         Owned<IDFAttributesIterator> iter = queryDistributedFileDirectory().getDFAttributesIterator("*",udesc,true,false);
+
+
+        StringAttr filters;
+        bool allMatchingFilesReceived;
+        Owned<IPropertyTreeIterator> fi = queryDistributedFileDirectory().getDFAttributesTreeIterator(filters.get(),
+            nullptr, nullptr, udesc, true, allMatchingFilesReceived);
+
+
         ForEach(*iter)
         {
             IPropertyTree &attr=iter->query();
@@ -2649,4 +2657,13 @@ ISashaServer *createSashaFileExpiryServer()
 #endif
     sashaExpiryServer = new CSashaExpiryServer(config);
     return sashaExpiryServer;
+}
+
+void testExpiry()
+{
+    Owned<IPropertyTree> config = serverConfig->getPropTree("DfuExpiry");
+    if (!config)
+        config.setown(createPTree("DfuExpiry"));
+    Owned<CSashaExpiryServer> sashaExpiryServer = new CSashaExpiryServer(config);
+    sashaExpiryServer->runExpiry();
 }
